bca8578ad7c28e5c3c083b8dd0dd3d17b010438f
Arsenijs <dingleee123@gmail.com>
2022-11-14 14:31:57 +0200

    rapapa

---
+++ main.py
@@ -0,0 +1,37 @@
+x = 4
+y = 6
+
+# 1)
+# print(5*10)
+
+# 4)
+# input1 = int(input("Ievadi pirmo ciparu:"))
+# input2 = int(input("Ievadi otro ciparu:"))
+# print(input1 + input2)
+# print(input1 * input2)
+# print(input1 / input2)
+
+# 5)
+# input4 = int(input("Ievadi skaitli:"))
+# if (input4 % 2 == 0):
+#     print("pāra skaitlis")
+
+# 6)
+# input_str = str(input("Ievadi tekstu:"))
+# output_str = input_str.replace("a" , "@")
+# output_str = output_str.replace ("l" , "I")
+# print(output_str)
+
+# 7)
+# input5 = str(input("Ievadi tekstu:"))
+# print(len(input5))
+
+# 8)
+# input6 = int(input("Ievadi pirmo skaitli:"))
+# if (input6 % 4 == 0 and input6 % 6 == 0):
+#     print("skaitlis dalās ar 4 un 6")
+# elif (input6 % 4 == 0 and input6 % 6 != 0 ):
+#     print("skaitlis dalās ar 4, bet nedalās ar 6")
+# else:
+#     print("sakitlis nedalās ar 4 vai 6")
+
